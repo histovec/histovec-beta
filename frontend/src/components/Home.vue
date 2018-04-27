ed864bc45b65cc80f791a8f17a321625c5960c0f
lepont <claire.lepont@interieur.gouv.fr>
2018-04-27 10:42:59 +0200

    substitution du header

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+    <main role="main">
+        <div class="col-12">
+            <h2 class="text-center transbox_2">Historique d'un véhicule</h2>
+            <div class="separator"></div>
+            <p class="text-center transbox_3">Acheter en toute confiance</p>
+            <p class="lead">
+            <div class="container">
+                <div class="row">
+                    <!-- debut bouton acheteur -->
+                    <div class="col-sm-6 text-center"> <a href="acheteur.html" class="btn radius-30 btn-dark btn-animated btn-lg">Acheteur <i class="fa fa-users"></i></a> </div>
+                    <!-- fin bouton acheteur -->
+                    <!-- debut bouton vendeurs -->
+                    <div class="col-sm-6 text-center">
+                        <div class="btn-group">
+                            <button type="button" class="btn radius-30 btn-default btn-lg dropdown-toggle" data-toggle="dropdown"> Vendeur </button>
+                            <div class="dropdown-menu"> <a class="dropdown-item" href="vendeur.html"><i class="fa fa-user pr-10"></i>Particulier</a> <a class="dropdown-item" href="vendeur.html"><i class="fa fa-building-o pr-10"></i>Entreprise</a> </div>
+                        </div>
+                    </div>
+                    <!-- fin bouton vendeurs -->
+                </div>
+            </div>
+            <div class="container text-center">
+                <div class="row">
+                    <!-- debut bouton en savoir plus -->
+                    <div class="col-lg-12"> <a href="en_savoir_plus.html" class="btn square btn-warning btn-sm m-g-15"><i class="fa fa-info pr-10"></i>En savoir plus...</a></div>
+                    <!-- fin bouton en savoir plus -->
+                </div>
+            </div>
+            </p>
+        </div>
+    </main>
+</template>
