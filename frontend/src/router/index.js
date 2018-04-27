50de4168c672f53995eca4b1d7c58ca19e0c3639
lepont <claire.lepont@interieur.gouv.fr>
2018-04-27 10:44:34 +0200

    ajout du Home

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+import Home from '@/components/Home'
 import Search from '@/components/Search'
 import Result from '@/components/Result'
 
@@ -9,7 +10,8 @@ Vue.use(Router)
 export default new Router({
   mode: 'history',
   routes: [
-    {name: 'root', path: '/', redirect: { name: 'search' }},
+    {name: 'root', path: '/', redirect: { name: 'home' }},
+    {name: 'home', path: '/home', component: Home},
     {name: 'search', path: '/search', component: Search},
     {name: 'result', path: '/result', component: Result}
   ]
