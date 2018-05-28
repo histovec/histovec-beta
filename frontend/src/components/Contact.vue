ac06c378061fc7569d423f3ad15362685331c150
antoinefa <fabien.antoine@m4x.org>
2018-05-28 09:03:18 +0200

    liens vers home pour contact et faq

--- frontend/src/components/Contact.vue
+++ frontend/src/components/Contact.vue
@@ -4,7 +4,7 @@
   <div class="breadcrumb-container">
     <div class="container">
       <ol class="breadcrumb">
-        <li><i class="fa fa-home pr-10"></i><a href="index.html">Accueil</a></li>
+        <li><i class="fa fa-home pr-10"></i><a href="home">Accueil</a></li>
         <li class="active">Contact</li>
       </ol>
     </div>
