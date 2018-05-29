40813895fc1d7de773e99a1f244712c5d5db2caf
lepont <claire.lepont@interieur.gouv.fr>
2018-05-29 14:17:22 +0200

    Ajout du lien vers la page acheteur

---
+++ frontend/src/components/Buyer.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <!-- breadcrumb start -->
+        <div class="breadcrumb-container">
+            <div class="container">
+                <ol class="breadcrumb">
+                    <li><i class="fa fa-home pr-10"></i><a href="home">Accueil</a></li>
+                    <li class="active">Acheteur</li>
+                </ol>
+            </div>
+        </div>
+        <!-- breadcrumb end -->
+        <!-- section -->
+        <section class="main-container">
+            <div class="container">
+                <div class="row">
+                    <!-- section start -->
+                    <section class="dark-translucent-bg fixed-bg pv-40" style="background-image:url(assets/images/acheteur_3.jpg);">
+                        <div class="container">
+                            <div class="row justify-content-lg-center">
+                                <div class="col-lg-12">
+                                    <h2 class="text-center mt-4"><span class="bold_6">Achetez</span> en confiance un <span class="bold_6">véhicule d'occassion</span></h2>
+                                    <div class="separator with-icon"><i class="fa fa-car bordered"></i></div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                    <!-- section end -->
+                    <!-- debut image fixe
+                    <div class="col-md-12"> <img src="assets/images/corporate-1-slider-slide-2.jpg" class="img-responsive" alt="slidebg2" data-bgposition="center top" data-bgrepeat="no-repeat" data-bgfit="cover"></div>
+              <!-- fin image fixe -->
+                </div>
+            </div>
+        </section>
+        <!-- section -->
+        <!-- main-container start -->
+        <section class="main-container">
+            <div class="container">
+                <div class="row">
+                    <!-- main start -->
+                    <div class="main col-md-12">
+                        <!-- page-title start -->
+                        <h3 class="title"><span class="text-defaut">A</span>cheteur</h3>
+                        <div class="separator-2"></div>
+                        <!-- page-title end -->
+                    </div>
+                    <!-- main end -->
+                </div>
+            </div>
+        </section>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 mb-20">
+                    
+                    <!-- accordion end -->
+                </div>
+            </div>
+            <!-- row -->
+        </div>
+        <!-- container -->
+    </div>
+</template>
