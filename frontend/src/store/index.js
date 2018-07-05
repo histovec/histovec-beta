d567bc5b0f6828290cdeb69ac085b14b7cb7be2e
antoinefa <fabien.antoine@m4x.org>
2018-07-05 23:03:16 +0200

    App: store fix

---
+++ frontend/src/store/index.js
@@ -0,0 +1,56 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  strict: process.env.NODE_ENV !== 'production',
+  state: {
+    typePersonne: 'particulier',
+    nom: '',
+    raisonSociale: '',
+    prenom: '',
+    dateNaissance: '',
+    plaque: '',
+    siren: '',
+    formule: '',
+    key: undefined,
+    code: undefined,
+    v: undefined
+  },
+  mutations: {
+    updateNom (state, nom) {
+      state.nom = nom
+    },
+    updatePrenom (state, prenom) {
+      state.prenom = prenom
+    },
+    updateRaisonSociale (state, raisonSociale) {
+      state.raisonSociale = raisonSociale
+    },
+    updateSiren (state, siren) {
+      state.siren = siren
+    },
+    updateDateNaissance (state, dateNaissance) {
+      state.dateNaissance = dateNaissance
+    },
+    updatePlaque (state, plaque) {
+      state.plaque = plaque
+    },
+    updateFormule (state, formule) {
+      state.formule = formule
+    },
+    updateTypePersonne (state, typePersonne) {
+      state.typePersonne = typePersonne
+    },
+    updateV (state, v) {
+      state.v = v
+    },
+    updateCode (state, code) {
+      state.code = code
+    },
+    updateKey (state, key) {
+      state.key = key
+    }
+  }
+})
