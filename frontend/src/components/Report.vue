88c05718ded5627f5b0be7fd24999209a25457ac
antoinefa <fabien.antoine@m4x.org>
2018-11-27 07:38:12 +0100

    gestion des opérations annulées: filtrage #395

--- frontend/src/components/Report.vue
+++ frontend/src/components/Report.vue
@@ -1058,6 +1058,7 @@ export default {
     histoFilter (historique) {
       let h = historique.filter(event => this.operations[event.opa_type] !== undefined)
       h = this.$lodash.orderBy(h, ['opa_date'], ['desc'])
+      h = h.filter(event => event.ope_annul === undefined)
       return h.map(event => {
         return {'date': this.formatDate(event.opa_date), 'nature': this.operations[event.opa_type]}
       })
