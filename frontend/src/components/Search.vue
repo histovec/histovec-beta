649a6b40a137a8562bdf8089177739e5cbd943a5
antoinefa <fabien.antoine@m4x.org>
2019-03-07 20:57:56 +0100

    Search: lean fonction semaine devenue inutile

--- frontend/src/components/Search.vue
+++ frontend/src/components/Search.vue
@@ -307,42 +307,9 @@ export default {
     },
     currentMonthNumber () {
       var date = new Date()
-      // patch temporaire pour la continuité du mois (réversibilité à faire mi-juillet)
-      // date = date.getFullYear() + '' + this.pad(date.getMonth() + 1, 2)
       date = moment().add(-7, 'days').format('YYYYMM')
       console.log(date)
       return date
-    },
-    currentWeekNumber () {
-      var instance = new Date()
-
-      // Create a copy of this date object
-      var target = new Date(instance.valueOf())
-
-      // ISO week date weeks start on monday
-      // so correct the day number
-      var dayNr = (instance.getDay() + 6) % 7
-
-      // ISO 8601 states that week 1 is the week
-      // with the first thursday of that year.
-      // Set the target date to the thursday in the target week
-      target.setDate(target.getDate() - dayNr + 3)
-
-      // Store the millisecond value of the target date
-      var firstThursday = target.valueOf()
-
-      // Set the target to the first thursday of the year
-      // First set the target to january first
-      target.setMonth(0, 1)
-      // Not a thursday? Correct the date to the next thursday
-      if (target.getDay() !== 4) {
-        target.setMonth(0, 1 + ((4 - target.getDay()) + 7) % 7)
-      }
-
-      // The currentWeekNumber is the number of weeks between the
-      // first thursday of the year and the thursday in the target week
-      var currentWeekNumber = 1 + Math.ceil((firstThursday - target) / 604800000)
-      return instance.getFullYear() + '' + currentWeekNumber
     },
     pers_id () {
       if (this.typePersonne === 'particulier') {
