e52a6c039eda68cfe3cd5035d741133027bad720
pli01 <pli01@noreply.github.com>
2019-04-01 22:38:29 +0200

    Add nginx log json. to activate in prod, must comment/uncomment access_log section

--- nginx/run.sh
+++ nginx/run.sh
@@ -10,6 +10,7 @@ set -e
  sed "s/<APP>/${APP}/g;s/<dataset>/${dataset}/g;s/<ES_HOST>/${ES_HOST}/g;s|<API_USER_BURST>|${API_USER_BURST}|g;s|<API_GLOBAL_BURST>|${API_GLOBAL_BURST}|g;" | \
  sed "/^server {/a\
 error_log /dev/stderr warn;\
+# access_log /dev/stdout main_json;\
 access_log /dev/stdout main;
 " > /etc/nginx/conf.d/default.conf
 sed "s|<API_GLOBAL_LIMIT_RATE>|${API_GLOBAL_LIMIT_RATE}|;s|<API_USER_LIMIT_RATE>|${API_USER_LIMIT_RATE}|g;s|<API_USER_SCOPE>|${API_USER_SCOPE}|g" < /etc/nginx/nginx.template > /etc/nginx/nginx.conf
