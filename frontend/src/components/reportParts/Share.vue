c3743461580bafde3f145539fe43add4ad6f45dd
Fabien ANTOINE <fabien.antoine@m4x.orga>
2019-04-03 03:08:04 +0200

    lint template and js of share

--- frontend/src/components/reportParts/Share.vue
+++ frontend/src/components/reportParts/Share.vue
@@ -1,32 +1,79 @@
 <template>
-  <div class="pv-30 ph-20 feature-box bordered_spec text-center" style="background: white">
+  <div
+    class="pv-30 ph-20 feature-box bordered_spec text-center" 
+    style="background: white"
+  >
     <div class="row">
       <!-- debut alerte verte -->
-      <div class="col-md-12" v-if="notifSuccess">
-        <div class="alert alert-icon alert-success" role="alert"><i class="fa fa-check"></i>Le lien a été copié</div>
+      <div 
+        v-if="notifSuccess"
+        class="col-md-12"
+      >
+        <div 
+          class="alert alert-icon alert-success"
+          role="alert"
+        >
+          <i class="fa fa-check"></i>Le lien a été copié
+        </div>
       </div>
       <!-- fin alerte verte -->
       <div class="col-md-12 p-h-10">
-        <p>Vous pouvez transmettre à votre acheteur potentiel, le rapport que vous venez de consulter par mail.<br>
-          Ce rapport sera accessible jusqu'au {{ validityDate }} <br>
+        <p>
+          Vous pouvez transmettre à votre acheteur potentiel, le rapport que vous venez de consulter par mail.
+          <br />
+          Ce rapport sera accessible jusqu'au {{ validityDate }} 
+          <br />
+        </p>
         <p class="text-center">
-          <button v-clipboard:copy="url" v-on:click="showNotifSuccess" class="btn radius-30 btn-dark btn-animated btn">Copier le lien <i class="fa fa-copy"></i></button>
-          <a :href="'mailto:?subject=Rapport%20Histovec&body=' + mailBody" class="btn radius-30 btn-default btn-animated btn">Courriel <i class="fa fa-send"></i></a>
+          <button 
+            v-clipboard:copy="url"
+            class="btn radius-30 btn-dark btn-animated btn"
+            @click="showNotifSuccess"
+          >
+            Copier le lien
+            <i class="fa fa-copy"></i>
+          </button>
+          <a
+            :href="'mailto:?subject=Rapport%20Histovec&body=' + mailBody"
+            class="btn radius-30 btn-default btn-animated btn"
+          >
+            Courriel
+            <i class="fa fa-send"></i>
+          </a>
         </p>
       </div>
       <div class="row">
-        <div class="col-md-12">Ou par QR code<p></p></div>
+        <div class="col-md-12">
+          Ou par QR code
+          <p></p>
+        </div>
       </div>
       <!-- <div class="separator"></div> -->
       <div class="row">
-        <div class="col-md-12" style="fload: none; margin: 0 auto">
-          <qrcode-vue :value="url" :size="150" level="L"></qrcode-vue>
+        <div
+          class="col-md-12"
+          style="fload: none; margin: 0 auto"
+        >
+          <qrcode-vue
+            :value="url"
+            :size="150"
+            level="L"
+          >
+          </qrcode-vue>
         </div>
       </div>
       <!-- debut bouton imprimer csa detaille -->
       <div class="row">
-        <div class="col-sm-12 pv-20" v-if="holder&&display['pdf']">
-          <csa :v="v" :url="url" :baseurl="baseurl"></csa>
+        <div 
+          v-if="holder&&display['pdf']"
+          class="col-sm-12 pv-20" 
+        >
+          <csa
+            :v="v"
+            :url="url"
+            :baseurl="baseurl"
+          >
+          </csa>
         </div>
       </div>
     <!-- fin bouton imprimer csa detaille -->
@@ -46,11 +93,19 @@ export default {
     Csa
   },
   props: {
-    v: Object,
-    url: String,
-    baseurl: String,
-    holder: Boolean,
-    display: Object
+    v: {
+      type: Object,
+      default: () => {}
+    },
+    url: {
+      type: String,
+      default: ''
+    },
+    baseurl: {
+      type: String,
+      default: ''
+    },
+    holder: Boolean
   },
   data () {
     return {
