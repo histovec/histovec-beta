6262992597dc45536638ed353c5fa9ce5902aff2
Stanislas Ormières <stan@stormier.ninja>
2019-04-03 15:47:17 +0200

    Use BASE_URL in router config
    - Keep it DRY
    - Prepare for an easy change in base URL

--- client/src/router.js
+++ client/src/router.js
@@ -15,17 +15,17 @@ Vue.use(Router)
 
 export default new Router({
   mode: 'history',
-  // base: process.env.BASE_URL, // TODO: Use this
+  base: process.env.BASE_URL || '/histovec',
   routes: [
-    { name: 'root', path: '/histovec/', redirect: { name: 'home' }, meta: { title: 'Histovec - Accueil' } },
-    { name: 'home', path: '/histovec/home', component: Home, meta: { title: 'Histovec - Accueil' } },
-    { name: 'search', path: '/histovec/search', component: Search, meta: { title: 'Histovec - Recherche' } },
-    { name: 'report', path: '/histovec/report', component: Report, meta: { title: 'Histovec - Rapport' } },
-    { name: 'faq', path: '/histovec/faq', component: Faq, meta: { title: 'Histovec - FAQ' } },
-    { name: 'legal', path: '/histovec/legal', component: Legal, meta: { title: 'Histovec - Mentions Légales' } },
-    { name: 'contact', path: '/histovec/contact', component: Contact, meta: { title: 'Histovec - Contact' } },
-    { name: 'feedback', path: '/histovec/feedback', component: Feedback, meta: { title: 'Histovec - Signaler un erreur' } },
-    { name: 'buyer', path: '/histovec/buyer', component: Buyer, meta: { title: 'Histovec - Acheteur' } },
-    { name: 'notfound', path: '/histovec/*', component: NotFound, meta: { title: 'Histovec - Page non trouvée' } }
+    { name: 'root', path: '/', redirect: { name: 'home' }, meta: { title: 'Histovec - Accueil' } },
+    { name: 'home', path: '/home', component: Home, meta: { title: 'Histovec - Accueil' } },
+    { name: 'search', path: '/search', component: Search, meta: { title: 'Histovec - Recherche' } },
+    { name: 'report', path: '/report', component: Report, meta: { title: 'Histovec - Rapport' } },
+    { name: 'faq', path: '/faq', component: Faq, meta: { title: 'Histovec - FAQ' } },
+    { name: 'legal', path: '/legal', component: Legal, meta: { title: 'Histovec - Mentions Légales' } },
+    { name: 'contact', path: '/contact', component: Contact, meta: { title: 'Histovec - Contact' } },
+    { name: 'feedback', path: '/feedback', component: Feedback, meta: { title: 'Histovec - Signaler un erreur' } },
+    { name: 'buyer', path: '/buyer', component: Buyer, meta: { title: 'Histovec - Acheteur' } },
+    { name: 'notfound', path: '/*', component: NotFound, meta: { title: 'Histovec - Page non trouvée' } }
   ]
 })
