d1ef11f9df7dc9e95c699fa153a92aac5fd45436
Fabien ANTOINE <fabien.antoine@m4x.orga>
2019-04-03 03:07:17 +0200

    lint template of history

--- frontend/src/components/reportParts/History.vue
+++ frontend/src/components/reportParts/History.vue
@@ -1,41 +1,67 @@
 <template>
   <div>
     <div>
-      Historique des opérations en France</div>
+      Historique des opérations en France
+    </div>
     <div class="row">
-      <div class="col-sm-4"><span class="txt-small-12"><h6>Date</h6></span></div>
-      <div class="col-sm-8"><span class="bold txt-small-12"><h6>Opération</h6></span></div>
+      <div class="col-sm-4">
+        <span class="txt-small-12"><h6>Date</h6></span>
+      </div>
+      <div class="col-sm-8">
+        <span class="bold txt-small-12"><h6>Opération</h6></span>
+      </div>
     </div>
     <div class="separator"></div>
 
-    <div v-for="(entry, index) in v.historique" :key="index">
+    <div 
+      v-for="(entry, index) in v.historique" 
+      :key="index"
+    >
       <div class="row">
-        <div class="col-sm-4"><span class="txt-small-12">{{ entry.date }}</span></div>
-        <div class="col-sm-8"><span class="info_red txt-small-12">{{ entry.nature }}</span></div>
+        <div class="col-sm-4">
+          <span class="txt-small-12">{{ entry.date }}</span>
+        </div>
+        <div class="col-sm-8">
+          <span class="info_red txt-small-12">{{ entry.nature }}</span>
+        </div>
       </div>
       <div class="separator pv-5"></div>
     </div>
     <div v-if="v.certificat.incertain">
       <div class="row">
-        <div class="col-sm-4"><span class="txt-small-12">{{ v.certificat.fr }}</span></div>
-        <div class="col-sm-8"><span class="info_red txt-small-12"> Première immatriculation (source incertaine)</span></div>
+        <div class="col-sm-4">
+          <span class="txt-small-12">{{ v.certificat.fr }}</span>
+        </div>
+        <div class="col-sm-8">
+          <span class="info_red txt-small-12"> Première immatriculation (source incertaine)</span>
+        </div>
       </div>
       <div class="separator pv-5"></div>
     </div>
     <!-- fin tableau operation historique FR -->
     <br />
-    <div v-if="v.certificat.etranger">Historique des opérations à l'étranger</div>
+    <div v-if="v.certificat.etranger">
+      Historique des opérations à l'étranger
+    </div>
     <!-- debut tableau operation historique Etranger -->
     <div v-if="v.certificat.etranger">
       <div class="row">
-        <div class="col-sm-4"><span class="txt-small-12"><h6>Date</h6></span></div>
-        <div class="col-sm-8"><span class="bold txt-small-12"><h6>Opération</h6></span></div>
+        <div class="col-sm-4">
+          <span class="txt-small-12"><h6>Date</h6></span>
+        </div>
+        <div class="col-sm-8">
+          <span class="bold txt-small-12"><h6>Opération</h6></span>
+        </div>
       </div>
       <div class="separator"></div>
       <div>
         <div class="row">
-          <div class="col-sm-4"><span class="txt-small-12">{{ v.certificat.premier }}</span></div>
-          <div class="col-sm-8"><span class="info_red txt-small-12">Première immatriculation à l'étranger</span></div>
+          <div class="col-sm-4">
+            <span class="txt-small-12">{{ v.certificat.premier }}</span>
+          </div>
+          <div class="col-sm-8">
+            <span class="info_red txt-small-12">Première immatriculation à l'étranger</span>
+          </div>
         </div>
         <div class="separator pv-5"></div>
       </div>
@@ -47,7 +73,10 @@
 
 export default {
   props: {
-    v: Object
+    v: {
+      type: Object,
+      default: () => {}
+    }
   }
 }
 
