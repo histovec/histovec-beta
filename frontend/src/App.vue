2d60f9da25836f3b410926785c01a6473ef872c3
Fabien ANTOINE <fabien.antoine@m4x.orga>
2019-04-03 02:58:21 +0200

    lint template of App

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,12 +8,35 @@
             <div class="col-md-8">
               <div class="header-left clearfix">
                 <!-- debut bandeau -->
-                <div class="logo"><a href="home"><img alt="accueil" @click="show = !show" src="assets/images/histovec-header-mobile.png" class="img-responsive" width="548" height="98"> </a></div>
+                <div class="logo">
+                  <a href="home">
+                    <img
+                      alt="accueil"
+                      src="assets/images/histovec-header-mobile.png"
+                      class="img-responsive"
+                      width="548"
+                      height="98"
+                      @click="show = !show"
+                    >
+                  </a>
+                </div>
                 <!-- fin bandeau -->
               </div>
             </div>
             <!-- debut beta -->
-            <div id="no-boot" class="col-xs-1" v-if="false"> <img alt="version beta" src="assets/images/beta.png" class="img-responsive" width="100" height="100"></div>
+            <div
+              v-if="display['beta']"
+              id="no-boot"
+              class="col-xs-1"
+            >
+              <img
+                alt="version beta"
+                src="assets/images/beta.png"
+                class="img-responsive"
+                width="100"
+                height="100"
+              >
+            </div>
             <!-- fin beta -->
           </div>
         </div>
@@ -22,7 +45,10 @@
     <!-- fin entete -->
     <router-view></router-view>
     <!-- footer start -->
-    <footer id="footer" class="clearfix dark">
+    <footer
+      id="footer"
+      class="clearfix dark"
+    >
       <!-- .footer start -->
       <div class="footer">
         <div class="container">
@@ -30,7 +56,9 @@
             <div class="row">
               <div class="col-md-6">
                 <div class="footer-content">
-                  <h4 class="title">Liens utiles</h4>
+                  <h4 class="title">
+                    Liens utiles
+                  </h4>
                   <div class="separator-2"></div>
                   <!-- debut -->
                   <div class="media margin-clear">
@@ -47,14 +75,40 @@
               </div>
               <div class="col-md-6">
                 <div class="footer-content">
-                  <h4 class="title">Un service proposé par</h4>
+                  <h4 class="title">
+                    Un service proposé par
+                  </h4>
                   <div class="separator-2"></div>
                   <div class="row grid-space-12">
                     <div class="col-md-2 p-b-10">
-                      <div class="overlay-container"> <img class="img-responsive" src="assets/images/logos_metiers/logo_mi.png" alt="Ministère de l'Intérieur"> <a href="https://www.interieur.gouv.fr/" target="_blank" class="overlay-link small"></a> </div>
+                      <div class="overlay-container">
+                        <img
+                          class="img-responsive"
+                          src="assets/images/logos_metiers/logo_mi.png"
+                          alt="Ministère de l'Intérieur"
+                        >
+                        <a
+                          href="https://www.interieur.gouv.fr/"
+                          target="_blank"
+                          class="overlay-link small"
+                        >
+                        </a>
+                      </div>
                     </div>
                     <div class="col-md-6 p-b-10">
-                      <div class="overlay-container"><img class="img-responsive" src="assets/images/logos_metiers/securite_routiere_120.png" alt="sécurité routière, tous responsables"> <a href="http://www.securite-routiere.gouv.fr/la-securite-routiere/actualites" target="_blank" class="overlay-link small"></a></div>
+                      <div class="overlay-container">
+                        <img
+                          class="img-responsive"
+                          src="assets/images/logos_metiers/securite_routiere_120.png"
+                          alt="sécurité routière, tous responsables"
+                        >
+                        <a
+                          href="http://www.securite-routiere.gouv.fr/la-securite-routiere/actualites"
+                          target="_blank"
+                          class="overlay-link small"
+                        >
+                        </a>
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -70,7 +124,10 @@
           <div class="subfooter-inner">
             <div class="row">
               <div class="col-md-12">
-                <p class="text-center">{{ appName }} version bêta {{ appVersion }} - copyright &copy; 2018 <a href="legal">Mentions légales</a></p>
+                <p class="text-center">
+                  {{ appName }} version bêta {{ appVersion }} - copyright &copy; 2018
+                  <a href="legal">Mentions légales</a>
+                </p>
               </div>
             </div>
           </div>
@@ -116,7 +173,13 @@ Vue.mixin({
       messages: messages,
       verbatims: verbatims,
       lang: localization.default,
-      show: false
+      show: false,
+      display: {
+        beta: false,
+        all_tabs: false,
+        pdf: true,
+        date_update: true
+      }
     }
   },
   created () {
@@ -139,7 +202,7 @@ Vue.mixin({
 })
 
 export default {
-  name: 'app'
+  name: 'App'
 }
 </script>
 
