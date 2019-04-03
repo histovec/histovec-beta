0a97aeed0b2b193230e06bbfe04c344b8fc0b20a
Stanislas Ormières <stan@stormier.ninja>
2019-04-03 16:42:23 +0200

    Create first story in storybook
    - Implement HomeBanner in its own component
    - Config storybook to import css and scss
    - Simplify the classes used

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -13,6 +13,12 @@ const viewport = {
 
 addParameters({ viewport });
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'font-awesome/css/font-awesome.css'
+import '@/assets/scss/_style.scss'
+import '@/assets/scss/bleu_vert.scss'
+import '@/assets/scss/style.scss'
+
 const req = require.context('../src', true, /.stories.js$/)
 
 function loadStories() {
