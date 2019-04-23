50e84f5877bcd4acbc265dca5484f42c9b884edb
Fabien ANTOINE <fabien.antoine@m4x.orga>
2019-04-23 22:05:33 +0200

    add custom minimal slugify for temporary replacement

---
+++ frontend/src/assets/js/slugify.js
@@ -0,0 +1,3 @@
+module.exports = function (str) {
+  return str.normalize('NFD').toLowerCase().replace(/[\u0300-\u036f]*/g, '').replace(/[^a-z0-9]+/gi, ' ').replace(/(^\s*|\s*)$/,'').replace(/\s+/gi,'-')
+}
