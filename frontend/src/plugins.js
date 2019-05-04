fdbdff305ae8ebe76da72407c7a3b8052db6a2c9
Fabien ANTOINE <fabien.antoine@m4x.orga>
2019-05-04 15:25:27 +0200

    refacto: supress vue-resource dependency

--- frontend/src/plugins.js
+++ frontend/src/plugins.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
-import VueResource from 'vue-resource'
 import VueShortkey from 'vue-shortkey'
 import VueScrollTo from 'vue-scrollto'
 
-Vue.use(VueResource)
 Vue.use(VueShortkey)
 Vue.use(VueScrollTo, {
   offset: -5,
