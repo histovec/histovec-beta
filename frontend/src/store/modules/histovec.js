06e2d80e022948198050819accd84c69a44aa60d
Fabien ANTOINE <fabien.antoine@m4x.orga>
2019-05-12 14:34:30 +0200

    frontend/add otc to store

--- frontend/src/store/modules/histovec.js
+++ frontend/src/store/modules/histovec.js
@@ -25,18 +25,43 @@ export default {
         state.id = id
         state.v = undefined
       }
+    },
+    cleanHistoVec (state) {
+      state.v = undefined
+      state.key = undefined
+      state.code = undefined
+      state.id = undefined
     }
   },
   actions: {
-    async getHistoVec ({ commit, state }) {
-      if (state.api && state.api.fetching) {
+    async getHistoVec ({ commit, state, rootState }, future) {
+      if (rootState.api && rootState.api.fetching && rootState.api.fetching.histovec) {
         return
       }
-      const response = await api.getHistoVec(state.id, state.key, localStorage.getItem('userId'))
+      let response
+      if (future) {
+        response = await api.getHistoVecV1(state.id, state.key, localStorage.getItem('userId'))
+      } else {
+        response = await api.getHistoVec(state.id, state.key, localStorage.getItem('userId'))
+      }
       if (response.success) {
         commit('updateV', response.v)
+        if (response.token) {
+          commit('updateToken', response.token)
+        }
       }
       return
+    },
+    async getHistoVecAndOtc ({ commit, state, rootState}) {
+      if (rootState.api && rootState.api.fetching && ( rootState.api.fetching.histovec || rootState.api.fetching.otc )) {
+        return
+      }
+      await api.getHistoVecAndOtc(state.id, state.key, rootState.identity.plaque, localStorage.getItem('userId'),
+        {
+          histovec: ((response) => { if (response.success) { commit('uptateV', response.v) } }),
+          otc: ((response) => { if (response.success) { commit('uptateCT', response.ct) } })
+        }
+      )
     }
   }
 }
