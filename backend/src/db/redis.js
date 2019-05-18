2ce667a37129ecaebb89ceef8b89e0e796352ef8
antoinefa <fabien.antoine@m4x.org>
2019-05-19 01:08:23 +0200

    backend/promisify redis client

--- backend/src/db/redis.js
+++ backend/src/db/redis.js
@@ -1,11 +1,11 @@
 // redis-client.js
 import redis from 'redis'
+import { promisify } from 'util'
 import { config } from '../config'
 const client = redis.createClient(config.redisUrl)
 
-export default {
-  async getAsync () { await client.get.bind(client) },
-  async setAsync () { await client.set.bind(client) },
-  async keysAsync () { await client.keys.bind(client) }
-}
+export const getAsync = promisify(client.get).bind(client)
+export const setAsync = promisify(client.set).bind(client)
+export const keysAsync = promisify(client.keys).bind(client)
 
+export default { getAsync, setAsync, keysAsync }
