7d630002543607f212390db36066312ec77727c6
antoinefa <fabien.antoine@m4x.org>
2019-05-19 01:08:47 +0200

    backend/set up redis for only in memory

--- backend/src/db/redis.js
+++ backend/src/db/redis.js
@@ -3,6 +3,7 @@ import redis from 'redis'
 import { promisify } from 'util'
 import { config } from '../config'
 const client = redis.createClient(config.redisUrl)
+client.config("SET", "save", "")
 
 export const getAsync = promisify(client.get).bind(client)
 export const setAsync = promisify(client.set).bind(client)
