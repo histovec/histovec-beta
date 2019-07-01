1f370e90222cf2216dbae181f2cd7c282ce58f0d
antoinefa <fabien.antoine@m4x.org>
2019-07-01 21:39:30 +0200

    add AB param for v1 activation in Makefile and docker

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -38,7 +38,10 @@ function isOptionActivated(uuid, pourcentage, base=16, precision=2) {
 }
 
 // @todo: waiting modal and mail content verification (and contact email in devops)
-const isBetaTestUser = isOptionActivated(localStorage.getItem('userId'), 0)
+const API_V1_PERCENTAGE = process.env.VUE_APP_V1_PERCENTAGE || 0
+const isBetaTestUser = isOptionActivated(localStorage.getItem('userId'), API_V1_PERCENTAGE)
+/* eslint-disable-next-line no-console */
+console.log(`API_V1_PERCENTAGE=${API_V1_PERCENTAGE}, isBetaTestUser=${isBetaTestUser}`)
 
 export default new Vuex.Store({
   strict: process.env.NODE_ENV !== 'production',
