134f3301007c7e9fb7f20f2adf422c19c053560a
Sébastien Nicolas <sebastien.nicolass@gmail.com>
2019-07-26 18:26:07 +0200

    :recycle: rename API_V1_PERCENTAGE as a generic AB testing parameter : AB_TESTING_PERCENTAGE

--- frontend/src/utils/abTesting.js
+++ frontend/src/utils/abTesting.js
@@ -1,7 +1,7 @@
 // Useful while AB testing with a feature flag
 // ex:
-// const API_V1_PERCENTAGE = process.env.VUE_APP_V1_PERCENTAGE || 0
-// const isBetaTestUser = isOptionActivated(localStorage.getItem('userId'), API_V1_PERCENTAGE)
+// const AB_TESTING_PERCENTAGE = process.env.VUE_APP_AB_TESTING_PERCENTAGE || 0
+// const isBetaTestUser = isOptionActivated(localStorage.getItem('userId'), AB_TESTING_PERCENTAGE)
 
 export function isOptionActivated(uuid, pourcentage, base=16, precision=2) {
   const subUuid = uuid.substring(0, precision)
