7bac2f22ac127945dc01d913e4b7bd1df1687f11
Sébastien Nicolas <sebastien.nicolass@gmail.com>
2020-11-18 17:03:48 +0100

    :bug: upgrade and use core-js and regenerator-runtime

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
 import 'es6-promise/auto'
 import Vue from 'vue'
 import App from './App'
