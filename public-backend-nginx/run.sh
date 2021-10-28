22c475dddbe102ebb70172202354f3a983c68c28
Sébastien Nicolas <sebastien.nicolass@gmail.com>
2021-10-28 16:50:17 +0200

    :sparkles: Add missing nginx for public-backend access

--- public-backend-nginx/run.sh
+++ public-backend-nginx/run.sh
@@ -7,9 +7,9 @@ set -e
 
 SED_REPLACE=`env | sed -e 's#\([^=]*\)=\(.*\)\s*$#s\#<\1>\#\2\#g;#'| tr '\n' ' ' | sed 's/$/\n/'`
 
-[ -z "${APP}" -o -z "${BACKEND_HOST}" -o -z "${BACKEND_PORT}" -o -z "${BACKEND_API_USER_BURST}" -o -z "${BACKEND_API_GLOBAL_BURST}" \
-  -o -z "${BACKEND_API_GLOBAL_LIMIT_RATE}" -o -z "${BACKEND_API_USER_LIMIT_RATE}" -o -z "${BACKEND_API_USER_SCOPE}" \
-  -o -z "${BACKEND_API_WRITE_LIMIT_RATE}" -o -z "${BACKEND_API_WRITE_BURST}" ] && exit 1
+[ -z "${APP}" -o -z "${PUBLIC_BACKEND_HOST}" -o -z "${PUBLIC_BACKEND_PORT}" -o -z "${PUBLIC_BACKEND_API_USER_BURST}" -o -z "${PUBLIC_BACKEND_API_GLOBAL_BURST}" \
+  -o -z "${PUBLIC_BACKEND_API_GLOBAL_LIMIT_RATE}" -o -z "${PUBLIC_BACKEND_API_USER_LIMIT_RATE}" -o -z "${PUBLIC_BACKEND_API_USER_SCOPE}" \
+  -o -z "${PUBLIC_BACKEND_API_WRITE_LIMIT_RATE}" ] && exit 1
 
 (
  cat /etc/nginx/conf.d/default.template | \
