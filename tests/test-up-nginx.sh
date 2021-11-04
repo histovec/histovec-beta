b959404f6816aae2fec4f6c91b83f5d6c1cb6957
Sébastien Nicolas <sebastien.nicolass@gmail.com>
2021-11-04 11:32:22 +0100

    :bug: revert frontend-nginx renaming

--- tests/test-up-nginx.sh
+++ tests/test-up-nginx.sh
@@ -8,7 +8,7 @@ basename=$(basename $0)
 echo "# $basename ${APP} ${APP_VERSION}"
 
 ret=0
-container_name=frontend-nginx-production
+container_name=nginx-production
 
 echo "# Wait ${APP}-$container_name up"
 set +e
