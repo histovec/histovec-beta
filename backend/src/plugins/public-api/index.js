a2438dda49a6df1f1c7a417deeb4a53de5bf4202
Sébastien Nicolas <sebastien.nicolass@gmail.com>
2022-07-31 02:52:47 +0200

    :wastebasket: remove useless logs

--- backend/src/plugins/public-api/index.js
+++ backend/src/plugins/public-api/index.js
@@ -1,8 +1,6 @@
 import reportByData from './routes/reportByData.js'
 import reportByCode from './routes/reportByCode.js'
 
-import { appLogger } from '../../util/logger.js'
-
 export const plugin = {
   name: 'publicApi',
   version: '1.0.0',
@@ -10,10 +8,7 @@ export const plugin = {
     const { apiPrefix } = options
 
     reportByData.path = apiPrefix + reportByData.path
-    appLogger.info(`-- [PUBLIC] -- API ROUTE => ${reportByData.path}`) // @todo: remove after validation in development environment
-
     reportByCode.path = apiPrefix + reportByCode.path
-    appLogger.info(`-- [PUBLIC] -- API ROUTE => ${reportByCode.path}`) // @todo: remove after validation in development environment
 
     const routes = [reportByData, reportByCode]
 
