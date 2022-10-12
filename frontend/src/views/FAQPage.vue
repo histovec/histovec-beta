bd617d4c4355e4db8bb5d40d5b74cfa6dd24dbe0
alexrignault <alex.rignault@ext.soprasteria.com>
2022-10-12 09:25:00 +0200

    Prise en compte retour: de le en du

--- frontend/src/views/FAQPage.vue
+++ frontend/src/views/FAQPage.vue
@@ -255,7 +255,7 @@ export default defineComponent({
               Seul le titulaire du certificat d'immatriculation en cours de validité peut générer le rapport HistoVec de son véhicule, qu'il choisit de transmettre à des tiers.
             </p>
             <p>
-              Si vous n’êtes pas le titulaire de le certificat d'immatriculation en cours de validité, vous ne pouvez pas consulter directement les données HistoVec pour ce véhicule. Il vous est possible de demander au propriétaire actuel la transmission par mél du rapport HistoVec en sélectionnant le profil acheteur sur la page d'accueil du site HistoVec.
+              Si vous n’êtes pas le titulaire du certificat d'immatriculation en cours de validité, vous ne pouvez pas consulter directement les données HistoVec pour ce véhicule. Il vous est possible de demander au propriétaire actuel la transmission par mél du rapport HistoVec en sélectionnant le profil acheteur sur la page d'accueil du site HistoVec.
             </p>
             <p>
               Les locataires longue durée (LDD) doivent être mandatés par le propriétaire du véhicule.
