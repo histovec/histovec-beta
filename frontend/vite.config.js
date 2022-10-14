c9b9db604965f277b53c8627ff68a6030cfabd5d
Lucas Ulivi <lucas.ulivi@soprasteria.com>
2022-10-14 18:12:14 +0200

    feat(); initialisation des tests unitaires

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,6 @@ import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import dotenv from 'dotenv'
 import vue from '@vitejs/plugin-vue'
-import svgLoader from 'vite-svg-loader'
 
 dotenv.config()
 
@@ -26,11 +25,6 @@ export default defineConfig({
   },
   plugins: [
     vue(),
-    svgLoader(
-    {
-      defaultImport: 'component',
-      multipass: true,
-    }),
   ],
   resolve: {
     alias: {
