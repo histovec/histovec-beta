f6d5941933e2f519b4f7611a3660eb50ec412f17
alexrignault <113456768+alexrignault@users.noreply.github.com>
2022-12-08 16:49:16 +0100

    fix(RGAA-1.2): Enlever l'alt sur FAQPage (#1374)

--- frontend/src/views/FAQPage.vue
+++ frontend/src/views/FAQPage.vue
@@ -64,7 +64,7 @@ export default defineComponent({
       />
     </div>
     <div class="fr-col-lg-4 fr-col-xl-4">
-      <ImagePresentation :src="image.faqSvg" alt="Illustration de la page de la FAQ" />
+      <ImagePresentation :src="image.faqSvg" />
     </div>
     <div class="fr-col-12  fr-col-lg-8  fr-col-xl-8  fr-mt-10v">
       <h1>Besoin d'aide ?</h1>
