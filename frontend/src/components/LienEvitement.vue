87ab3ba3d9d4143097d987e991568bf3d23c2150
alexrignault <alex.rignault@ext.soprasteria.com>
2022-12-12 10:19:15 +0100

    fix(RGAA-12.7): Lien d'accès rapide

---
+++ frontend/src/components/LienEvitement.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="fr-skiplinks">
+    <nav class="fr-container" role="navigation" aria-label="Accès rapide">
+      <ul class="fr-skiplinks__list">
+        <li>
+          <a class="fr-link" href="#contenu">Contenu</a>
+        </li>
+        <li>
+          <a class="fr-link" href="#button-menu-header">Menu</a>
+        </li>
+        <li>
+          <a class="fr-link" href="#footer">Pied de page</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</template>
