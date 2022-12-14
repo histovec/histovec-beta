377502ed4c396d12d7c14b8080d8eb42cc39f64e
alexrignault <113456768+alexrignault@users.noreply.github.com>
2022-12-14 10:09:32 +0100

    fix(RGAA-6.1): ajouter un titre sur les liens UTAC et sa signification (#1385)

--- frontend/src/views/FAQPage.vue
+++ frontend/src/views/FAQPage.vue
@@ -842,10 +842,11 @@ export default defineComponent({
             <p>
               En cas d’absence de contrôle technique ou de relevé de kilométrage, nous vous invitons à consulter <a
                 class="fr-link"
+                title="Le site utac-otc.com"
                 href="https://www.utac-otc.com/infos-pratiques/acc%C3%A8s-par-besoin"
                 rel="noopener noreferrer"
                 target="_blank"
-              >le site de l'UTAC</a>.
+              >le site de l'UTAC</a> (Union technique de l'automobile, du motocycle et du cycle).
             </p>
           </DsfrAccordion>
         </li>
@@ -880,10 +881,11 @@ export default defineComponent({
             <p>
               Pour plus d'informations, vous pouvez consulter <a
                 class="fr-link"
+                title="Le site utac-otc.com"
                 href="https://www.utac-otc.com/infos-pratiques/acc%C3%A8s-par-besoin"
                 rel="noopener noreferrer"
                 target="_blank"
-              >le site de l'UTAC</a>.
+              >le site de l'UTAC</a> (Union technique de l'automobile, du motocycle et du cycle).
             </p>
           </DsfrAccordion>
         </li>
