e25711e679555b46f95d191181e8dcbe8dce5c05
alexrignault <alex.rignault@ext.soprasteria.com>
2023-01-04 18:31:17 +0100

    fix(RGAA-12.8): modifier ordre focus sur la modale de RapportPage

--- frontend/src/components/HistoVecModale.vue
+++ frontend/src/components/HistoVecModale.vue
@@ -1,8 +1,12 @@
 <script>
 import { defineComponent } from 'vue'
+import { FocusTrap } from 'focus-trap-vue'
 
 export default defineComponent({
   name: 'HistoVecModale',
+  components: {
+    FocusTrap,
+  },
   inheritAttrs: false,
   props: {
     titre: {
@@ -48,48 +52,51 @@ export default defineComponent({
 </script>
 
 <template>
-  <dialog
-    id="fr-modal-1"
-    aria-labelledby="fr-modal-title-modal-1"
-    class="fr-modal"
-    :class="{'fr-modal--opened': opened}"
+  <focus-trap
+    v-if="opened"
   >
-    <div class="fr-container fr-container--fluid fr-container-md">
-      <div class="fr-grid-row fr-grid-row--center">
-        <div class="fr-col-12 fr-col-md-8 fr-col-lg-6">
-          <div class="fr-modal__body">
-            <div class="fr-modal__header">
-              <button
-                ref="closeBtn"
-                class="fr-btn--close fr-btn"
-                aria-controls="fr-modal-1"
-                title="Fermer la fenêtre modale"
-                role="button"
-                @click="close()"
-              >
-                Fermer
-              </button>
-            </div>
-            <div class="fr-modal__content">
-              <h1
-                id="fr-modal-title-modal-1"
-                class="fr-modal__title"
-              >
-                {{ titre }}
-              </h1>
-              <slot />
-            </div>
-            <div class="fr-modal__footer">
-              <DsfrButtonGroup
-                align="right"
-                :buttons="actions"
-                inline
-                reverse
-              />
+    <dialog
+      id="fr-modal-1"
+      aria-labelledby="fr-modal-title-modal-1"
+      class="fr-modal"
+      :class="{'fr-modal--opened': opened}"
+    >
+      <div class="fr-container fr-container--fluid fr-container-md">
+        <div class="fr-grid-row fr-grid-row--center">
+          <div class="fr-col-12 fr-col-md-8 fr-col-lg-6">
+            <div class="fr-modal__body">
+              <div class="fr-modal__header">
+                <button
+                  ref="closeBtn"
+                  class="fr-btn--close fr-btn"
+                  aria-controls="fr-modal-1"
+                  title="Fermer la fenêtre modale"
+                  role="button"
+                  @click="close()"
+                >
+                  Fermer
+                </button>
+              </div>
+              <div class="fr-modal__content">
+                <h1
+                  id="fr-modal-title-modal-1"
+                  class="fr-modal__title"
+                >
+                  {{ titre }}
+                </h1>
+                <slot />
+              </div>
+              <div class="fr-modal__footer">
+                <DsfrButtonGroup
+                  align="right"
+                  :buttons="actions"
+                  inline
+                />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  </dialog>
+    </dialog>
+  </focus-trap>
 </template>
