d2c1841391000ffc85dd13edee0bb5720f90e114
alexrignault <alex.rignault@ext.soprasteria.com>
2023-01-17 16:55:47 +0100

    fix(RGAA-8.2): suppression caracteres illegaux relevé par w3c

--- frontend/src/utils/email.js
+++ frontend/src/utils/email.js
@@ -5,7 +5,5 @@ export const mailTo = ({ recipients, subject, body }) => {
 		})
 		.join(',');
 
-	return `mailto:${concatenedRecipients || ''}
-	?subject=${subject ? encodeURIComponent(subject) : ''}
-	&body=${body ? encodeURIComponent(body) : ''}`;
+	return `mailto:${concatenedRecipients || ''}?subject=${subject ? encodeURIComponent(subject) : ''}&body=${body ? encodeURIComponent(body) : ''}`;
 };
