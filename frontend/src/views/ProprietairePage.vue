1f02ed386a65dbb43352b3ad46b88a722447ea34
alexrignault <alex.rignault@ext.soprasteria.com>
2023-01-27 10:32:11 +0100

    fix(RGAA-8.2): validator w3c sur ProprietairePage

--- frontend/src/views/ProprietairePage.vue
+++ frontend/src/views/ProprietairePage.vue
@@ -65,11 +65,15 @@ export default defineComponent({
       }
     )
     const mentionChampObligatoire ='Les champs marqués d\'un astérisque sont obligatoires.'
+    const tabSivTitles = [{ title: 'Particulier', panelId: 'siv-tab-content-0', tabId:'siv-tab-0'}, { title: 'Personne morale', panelId: 'siv-tab-content-1', tabId:'siv-tab-1'}]
+    const tabFniTitles = [{ title: 'Particulier', panelId: 'fni-tab-content-0', tabId:'fni-tab-0'}, { title: 'Personne morale', panelId: 'fni-tab-content-1', tabId:'fni-tab-1'}]
 
     return {
       formData,
       collerPressePapierEtDistribuerDansFormulaire: collerPressePapierEtDistribuerDansFormulaire,
       CHAMP_MODIFIE: CHAMP_MODIFIE,
+      tabSivTitles,
+      tabFniTitles,
       mentionChampObligatoire,
       modals: {
         common: {
@@ -770,7 +774,7 @@ export default defineComponent({
     >
       <DsfrTabs
         tab-list-name="Liste d'onglets pour un véhicule avec un numéro d'immatriculation au format S&#8203;I&#8203;V"
-        :tab-titles="[{ title: 'Particulier'}, { title: 'Personne morale'}]"
+        :tab-titles="tabSivTitles"
         @select-tab="selectSivTab"
       >
         <DsfrTabContent
@@ -802,7 +806,6 @@ export default defineComponent({
                   aria-required="true"
                   :aria-invalid="!isNomSivValid"
                   aria-errormessage="nom-particulier-SIV-erreur-message"
-                  aria-describedby="nom-particulier-SIV-erreur-message"
                 >
                   <template #required-tip>
                     <em class="required-label"> *</em>
@@ -836,7 +839,6 @@ export default defineComponent({
                   aria-required="true"
                   :aria-invalid="!isPrenomsSivValid"
                   aria-errormessage="prenom-particulier-SIV-erreur-message"
-                  aria-describedby="prenom-particulier-SIV-erreur-message"
                 >
                   <template #required-tip>
                     <em class="required-label"> *</em>
@@ -878,7 +880,6 @@ export default defineComponent({
                   aria-required="true"
                   :aria-invalid="!isNumeroImmatriculationSivValid"
                   aria-errormessage="numero-immatriculation-particulier-SIV-erreur-message"
-                  aria-describedby="numero-immatriculation-particulier-SIV-erreur-message"
                 >
                   <template #required-tip>
                     <em class="required-label"> *</em>
@@ -914,7 +915,6 @@ export default defineComponent({
                   aria-required="true"
                   :aria-invalid="!isNumeroFormuleSivValid"
                   aria-errormessage="numero-formule-particulier-SIV-erreur-message"
-                  aria-describedby="numero-formule-particulier-SIV-erreur-message"
                 >
                   <template #required-tip>
                     <em class="required-label"> *</em>
@@ -966,7 +966,6 @@ export default defineComponent({
                   aria-required="true"
                   :aria-invalid="!isRaisonSocialeSivValid"
                   aria-errormessage="raison-sociale-personne-morale-SIV-erreur-message"
-                  aria-describedby="raison-sociale-personne-morale-SIV-erreur-message"
                 >
                   <template #required-tip>
                     <em class="required-label"> *</em>
@@ -988,7 +987,6 @@ export default defineComponent({
                   @paste="collerPressePapierEtDistribuerDansFormulaire(formData, CHAMP_MODIFIE.SIV_SIREN, $event)"
                   :aria-invalid="!isNumeroSirenSivValid"
                   aria-errormessage="numero-siren-personne-morale-SIV-erreur-message"
-                  aria-describedby="numero-siren-personne-morale-SIV-erreur-message"
                 >
                   <template #required-tip>
                     <button
@@ -1029,7 +1027,6 @@ export default defineComponent({
                   aria-required="true"
                   :aria-invalid="!isNumeroImmatriculationSivValid"
                   aria-errormessage="numero-immatriculation-personne-morale-SIV-erreur-message"
-                  aria-describedby="numero-immatriculation-personne-morale-SIV-erreur-message"
                 >
                   <template #required-tip>
                     <em class="required-label"> *</em>
@@ -1065,7 +1062,6 @@ export default defineComponent({
                   aria-required="true"
                   :aria-invalid="!isNumeroFormuleSivValid"
                   aria-errormessage="numero-formule-personne-morale-SIV-erreur-message"
-                  aria-describedby="numero-formule-personne-morale-SIV-erreur-message"
                 >
                   <template #required-tip>
                     <em class="required-label"> *</em>
@@ -1095,7 +1091,7 @@ export default defineComponent({
     >
       <DsfrTabs
         tab-list-name="Liste d'onglets pour un véhicule avec un numéro d'immatriculation au format FNI"
-        :tab-titles="[{ title: 'Particulier'}, { title: 'Personne morale'}]"
+        :tab-titles="tabFniTitles"
         @select-tab="selectFniTab"
       >
         <DsfrTabContent
@@ -1127,7 +1123,6 @@ export default defineComponent({
                   aria-required="true"
                   :aria-invalid="!isNomEtPrenomsFniValid"
                   aria-errormessage="nom-et-prenom-particulier-FNI-erreur-message"
-                  aria-describedby="nom-et-prenom-particulier-FNI-erreur-message"
                 >
                   <template #required-tip>
                     <em class="required-label"> *</em>
@@ -1170,7 +1165,6 @@ export default defineComponent({
                   aria-required="true"
                   :aria-invalid="!isNumeroImmatriculationFniValid"
                   aria-errormessage="numero-immatriculation-particulier-FNI-erreur-message"
-                  aria-describedby="numero-immatriculation-particulier-FNI-erreur-message"
                 >
                   <template #required-tip>
                     <em class="required-label"> *</em>
@@ -1206,7 +1200,6 @@ export default defineComponent({
                   aria-required="true"
                   :aria-invalid="!isDateEmissionCertificatImmatriculationFniValid"
                   aria-errormessage="date-emission-certificat-immatriculation-particulier-FNI-erreur-message"
-                  aria-describedby="date-emission-certificat-immatriculation-particulier-FNI-erreur-message"
                 >
                   <template #required-tip>
                     <em class="required-label"> *</em>
@@ -1258,7 +1251,6 @@ export default defineComponent({
                   aria-required="true"
                   :aria-invalid="!isRaisonSocialeFniValid"
                   aria-errormessage="raison-sociale-personne-morale-FNI-erreur-message"
-                  aria-describedby="raison-sociale-personne-morale-FNI-erreur-message"
                 >
                   <template #required-tip>
                     <em class="required-label"> *</em>
@@ -1280,7 +1272,6 @@ export default defineComponent({
                   @paste="collerPressePapierEtDistribuerDansFormulaire(formData, CHAMP_MODIFIE.FNI_SIREN, $event)"
                   :aria-invalid="!isNumeroSirenFniValid"
                   aria-errormessage="numero-siren-personne-morale-FNI-erreur-message"
-                  aria-describedby="numero-siren-personne-morale-FNI-erreur-message"
                 >
                   <template #required-tip>
                     <button
@@ -1322,7 +1313,6 @@ export default defineComponent({
                   aria-required="true"
                   :aria-invalid="!isNumeroImmatriculationFniValid"
                   aria-errormessage="numero-immatriculation-personne-morale-FNI-erreur-message"
-                  aria-describedby="numero-immatriculation-personne-morale-FNI-erreur-message"
                 >
                   <template #required-tip>
                     <em class="required-label"> *</em>
@@ -1358,7 +1348,6 @@ export default defineComponent({
                   aria-required="true"
                   :aria-invalid="!isDateEmissionCertificatImmatriculationFniValid"
                   aria-errormessage="date-emission-certificat-immatriculation-personne-morale-FNI-erreur-message"
-                  aria-describedby="date-emission-certificat-immatriculation-personne-morale-FNI-erreur-message"
                 >
                   <template #required-tip>
                     <em class="required-label"> *</em>
