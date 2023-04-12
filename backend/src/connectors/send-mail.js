07ffc25c269e18e635b2b962d22a5bf089c916f4
alexrignault <alex.rignault@ext.soprasteria.com>
2023-04-12 16:34:06 +0200

    afficher uuid dans les logs

--- backend/src/connectors/send-mail.js
+++ backend/src/connectors/send-mail.js
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer'
 import config from '../config.js'
 import { syslogLogger } from '../util/logger.js'
 
-export const sendMail = async ({ from, to, cc, subject, content: html }) => {
+export const sendMail = async ({ from, to, cc, subject, uuid, content: html }) => {
   let transporter
 
   try {
@@ -35,13 +35,13 @@ export const sendMail = async ({ from, to, cc, subject, content: html }) => {
       subject,
       html,
     })
-    syslogLogger.info({ key: 'Mail sent:', tag: 'MAIL', value: { messageId: info.messageId, reponse: info.response } })
+    syslogLogger.info({ key: 'Mail sent:', tag: 'MAIL', uuid, value: { messageId: info.messageId, reponse: info.response } })
     if (isMailerMocked) {
       // Preview only available when sending through an Ethereal account
-      syslogLogger.debug({ key: 'Preview URL for email: ', tag: 'MAIL', value: { lien: nodemailer.getTestMessageUrl(info) } })
+      syslogLogger.debug({ key: 'Preview URL for email: ', tag: 'MAIL', uuid, value: { lien: nodemailer.getTestMessageUrl(info) } })
     }
   } catch (error) {
-    syslogLogger.error({ key: '❌ Error while sending mail : ', tag: 'MAIL', value: error })
+    syslogLogger.error({ key: '❌ Error while sending mail : ', tag: 'MAIL', uuid, value: error })
     throw error
   } finally {
     transporter.close()
