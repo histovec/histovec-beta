bfa24a9961390e96326b9e2ab83a6b5babe25dd3
Lucas Ulivi <111894964+lucas-ulivi@users.noreply.github.com>
2023-05-04 17:10:40 +0200

    feat: initialisation du service d'appel à l'api data (#1471)

---
+++ backend/src/services/api-data.js
@@ -0,0 +1,51 @@
+import axios from 'axios'
+import config from '../config.js'
+
+export class ApiDataCLient {
+  constructor () {
+    // utiliser cette variable pour gérer le bouchon de l'appel à l'API data
+    // const bouchonActive = config.apiData.isMocked
+    const baseURL = config.apiData.apiUrl
+
+    const options = {
+      baseURL,
+      headers: {
+        header: 'application/json',
+      },
+    }
+
+    this.axios = axios.create(options)
+  }
+
+  get = async (url) => {
+    return await this.axios.get(url)
+      .then(response => {
+        return response.data
+      })
+      .catch(error => {
+        throw error
+      })
+  }
+
+  post = async (url, data) => {
+    return await this.axios.post(url, data)
+      .then(response => {
+        return response.data
+      })
+      .catch(error => {
+        throw error
+      })
+  }
+
+  getByCode = async (uuid, clefAcheteur) => {
+    return await this.get('/report_by_code/' + uuid + '/' + clefAcheteur)
+  }
+
+  getSivPhysique = async (uuid, data) => {
+    return await this.post('/report_by_params/siv/physique/' + uuid, data)
+  }
+
+  getDictionnaireOpaType = async () => {
+    return await this.get('/dicos/opa_type')
+  }
+}
