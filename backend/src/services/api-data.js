027abfa15930d1848dd732d4e9d3f91acd2cff51
alexrignault <113456768+alexrignault@users.noreply.github.com>
2023-05-16 17:59:06 +0200

    feat():initialisation du bouchon apidata (#1468)

--- backend/src/services/api-data.js
+++ backend/src/services/api-data.js
@@ -1,10 +1,12 @@
 import axios from 'axios'
 import config from '../config.js'
+import { SIV_PHYSIQUE, SIV_PHYSIQUE_MIN } from '../constant/bouchon/siv_physique.js'
+
+// utiliser cette variable pour gérer le bouchon de l'appel à l'API data
+const bouchonActive = config.apiData.isSIVMockActivated
 
 export class ApiDataCLient {
   constructor () {
-    // utiliser cette variable pour gérer le bouchon de l'appel à l'API data
-    // const bouchonActive = config.apiData.isMocked
     const baseURL = config.apiData.apiUrl
 
     const options = {
@@ -38,10 +40,32 @@ export class ApiDataCLient {
   }
 
   getByCode = async (uuid, clefAcheteur) => {
+    if (bouchonActive) {
+      return {
+        status: SIV_PHYSIQUE.status,
+        message: SIV_PHYSIQUE.message,
+        payload: SIV_PHYSIQUE.payload,
+      }
+    }
     return await this.get('/report_by_code/' + uuid + '/' + clefAcheteur)
   }
 
   getSivPhysique = async (uuid, data) => {
+    if (bouchonActive) {
+      if (data.nom === 'vehiculeMin') {
+        return {
+          status: SIV_PHYSIQUE_MIN.status,
+          message: SIV_PHYSIQUE_MIN.message,
+          payload: SIV_PHYSIQUE_MIN.payload,
+        }
+      } else {
+        return {
+          status: SIV_PHYSIQUE.status,
+          message: SIV_PHYSIQUE.message,
+          payload: SIV_PHYSIQUE.payload,
+        }
+      }
+    }
     return await this.post('/report_by_data/siv/physique/' + uuid, data)
   }
 
