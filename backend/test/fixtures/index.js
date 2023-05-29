d482fae707a98b585e8cf99c8993ccb9925c2c13
Lucas Ulivi <111894964+lucas-ulivi@users.noreply.github.com>
2023-05-29 16:31:14 +0200

    feat: ajout de la vérification du format de réponse de l'api data (#1477)

---
+++ backend/test/fixtures/index.js
@@ -0,0 +1,25 @@
+import { reponseApiDataCode200 } from './apiData/status_200.js'
+import { reponseApiDataCode204 } from './apiData/status_204.js'
+import { reponseApiDataCode205 } from './apiData/status_205.js'
+import { reponseApiDataIncomingQueryManquant } from './apiData/incomingQuery_manquant.js'
+import { reponseApiDataIncomingQueryImmatManquant } from './apiData/incomingQuery_immat_manquant.js'
+import { reponseApiDataIncomingQueryImmatDifferent } from './apiData/incomingQuery_immat_different.js'
+import { reponseApiDataControleTechniqueImmatManquant } from './apiData/controleTechnique_immat_manquant.js'
+import { reponseApiDataControleTechniqueImmatDifferent } from './apiData/controleTechnique_immat_different.js'
+import { reponseApiDataControleTechniqueManquant } from './apiData/controleTechnique_manquant.js'
+import { reponseApiDataVehiculeVole } from './apiData/vehiculeVole.js'
+import { reponseApiDataDateMauvaisFormat } from './apiData/infos_datePremiereImmat_mauvaisFormat.js'
+
+export {
+  reponseApiDataCode200,
+  reponseApiDataCode204,
+  reponseApiDataCode205,
+  reponseApiDataIncomingQueryManquant,
+  reponseApiDataIncomingQueryImmatManquant,
+  reponseApiDataIncomingQueryImmatDifferent,
+  reponseApiDataControleTechniqueImmatManquant,
+  reponseApiDataControleTechniqueImmatDifferent,
+  reponseApiDataControleTechniqueManquant,
+  reponseApiDataVehiculeVole,
+  reponseApiDataDateMauvaisFormat
+}
