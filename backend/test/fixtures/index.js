d90908961a015f6f2be746e4eba9622e36919f7a
Lucas Ulivi <111894964+lucas-ulivi@users.noreply.github.com>
2023-06-08 12:03:06 +0200

    feat: modification du bouchou et des mapper pour ajouter gages, dvs, … (#1483)

--- backend/test/fixtures/index.js
+++ backend/test/fixtures/index.js
@@ -1,4 +1,12 @@
 import { reponseApiDataCode200 } from './apiData/status_200.js'
+import { reponseApiDataCode200SansGages } from './apiData/status_200_sansGages.js'
+import { reponseApiDataCode200SansDvs } from './apiData/status_200_sansDvs.js'
+import { reponseApiDataCode200SansSuspensions } from './apiData/status_200_sansSuspensions.js'
+import { reponseApiDataCode200SansOppositions } from './apiData/status_200_sansOppositions.js'
+import { reponseApiDataCode200SansOves } from './apiData/status_200_sansOves.js'
+import { reponseApiDataCode200SansOveis } from './apiData/status_200_sansOveis.js'
+import { reponseApiDataCode200SansOtcisPv } from './apiData/status_200_sansOtcisPv.js'
+import { reponseApiDataCode200SansOtcis } from './apiData/status_200_sansOtcis.js'
 import { reponseApiDataCode204 } from './apiData/status_204.js'
 import { reponseApiDataCode205 } from './apiData/status_205.js'
 import { reponseApiDataIncomingQueryManquant } from './apiData/incomingQuery_manquant.js'
@@ -12,6 +20,10 @@ import { reponseApiDataDateMauvaisFormat } from './apiData/infos_datePremiereImm
 
 export {
   reponseApiDataCode200,
+  reponseApiDataCode200SansGages,
+  reponseApiDataCode200SansDvs,
+  reponseApiDataCode200SansSuspensions,
+  reponseApiDataCode200SansOppositions,
   reponseApiDataCode204,
   reponseApiDataCode205,
   reponseApiDataIncomingQueryManquant,
@@ -21,5 +33,9 @@ export {
   reponseApiDataControleTechniqueImmatDifferent,
   reponseApiDataControleTechniqueManquant,
   reponseApiDataVehiculeVole,
-  reponseApiDataDateMauvaisFormat
+  reponseApiDataDateMauvaisFormat,
+  reponseApiDataCode200SansOves,
+  reponseApiDataCode200SansOveis,
+  reponseApiDataCode200SansOtcisPv,
+  reponseApiDataCode200SansOtcis,
 }
