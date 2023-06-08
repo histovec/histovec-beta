2a7a8628a3a9ad59fb70815b230582655aff745d
Lucas Ulivi <111894964+lucas-ulivi@users.noreply.github.com>
2023-06-08 12:03:06 +0200

    feat: modification du bouchou et des mapper pour ajouter gages, dvs, … (#1483)

--- backend/src/constant/bouchon/mock_historique.js
+++ backend/src/constant/bouchon/mock_historique.js
@@ -1,574 +1,570 @@
 export const HISTORIQUE = [
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'CUMUL_OPERATIONS',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'IMMAT_DIPLO_DEMANDE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'IMMAT_DIPLO_CONFIRMATION',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'IMMAT_DIPLO_INFIRMATION',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'IMMAT_DIPLO_RETIRER',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'IMMAT_DIPLO_PREM_VO_CONF',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'IMMAT_NORMALE_PREM_ETRANGER',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'IMMAT_NORMALE_PREM_VO',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'IMMAT_NORMALE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'IMMAT_NORMALE_SOURCE_INCERTAINE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'IVN_CARROSSIER_QUALIFIE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'IMMAT_WG',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'IMMAT_PROVISOIRE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'RENOUV_WG',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'PROROG_IMMAT',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'CHANG_TIT_NORMAL',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'CHANG_TIT_PREDEMANDE_INTERNET',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'CHANG_TIT_DIPLO',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'CHANG_LOC',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'MODIFIER_USAGES',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'FIN_USAGE_DEMO_VENTE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'FIN_USAGE_DEMO_FINDEM',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'PROROG_USAGE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'ATT_NUM_USURPATION',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'MODIF_ADRESSE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'MODIF_ADRESSE_DGME',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'MODIF_ADRESSE_LOCATAIRE_PRO',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'MODIF_ETAT_CIVIL',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'REIMMAT_ETRANGER',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'RETOUR_REIMMAT_ETR',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'RETRAIT_VOLONTAIRE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'REMISE_CIRC_RETRAIT_VOL',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'SORTIE_TERRITOIRE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'REMISE_CIRC_SORTIE_TERR',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'DUPLICATA',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'DEC_VE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'PREM_RAP_VE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'SEC_RAP_VE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'INSCRIPTION_GAGE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'CESSION_GAGE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'RADIATION_GAGE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'PROROG_GAGE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'DECLARATION_DESTRUCTION',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'DESTRUCTION_VEHICULE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'DECLARATION_ACHAT',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'DECLARATION_CESSION',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'DECLARATION_CESSION_DGME',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'INSCRIRE_OTCI',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'LEVER_OTCI',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'INSCRIRE_OPPOSITION',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'LEVER_OPPOSITION',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'INSCRIRE_DVS',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'LEVER_DVS',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'RENOUV_DVS',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'RELANCE_PROD_DOC',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'PROD_TITRE_HORS_DUPLICATA',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'RESTITUTION_TITRE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'DECLARATION_PERTE_TITRE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'EDITION_CPI',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'MODIF_CARAC_TECH',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'REMISE_LOT_TITRE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'REMISE_TITRE_FO',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'REMISE_TITRE_PREF',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'IMMOBILISATION_VEHICULE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'IMMOBILISATION_VEHICULE_ADM',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'LEVEE_IMMOBILISATION_VEHICULE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'INSCRIRE_MSG_SERVICE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'INCONU_REPRISE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'REPRISE_DP7_1993',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'CONVERSION_DOSSIER_FNI',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'CORRECTION_DOSSIER',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'CORRECTION_SIT_ADMIN',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'CORRECTION_TITULAIRE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'CORRECTION_VEHICULE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'CORRECTION_TITRE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'CORRECTION_AUTORISATION',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'PROD_DOC',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'INSCRIRE_OVE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'LEVER_OVE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'MODIF_DROIT_OPPOSITION',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'ATTRIBUTION_CODE_CONF',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'VEHICULE_REFCNIT_RUBVARIABLES',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'VEHICULE_REFCNIT_MENTIONVARIABLE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'RECHERCHE_DOSSIERS',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'CONSULT_DOSSIER',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'ANNULATION_OPERATION',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'MAJ_PARAM_FONC',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'ADMIN_NOMENC',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'CREA_COMPT_UTIL',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'MODIF_COMPT_UTIL',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'INVA_COMPT_UTIL',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'SUSP_REAC_COMPT_UTIL',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'CREA_COMPT_PART',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'MODIF_COMPT_PART',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'SUSP_REAC_COMPT_PART',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'ACTIVATION_COMPT_PART',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'SUSPENSION_COMPT_PART',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'RAD_COMPT_PART',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'RETRAIT_COMPT_PART',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'RESILIATION_COMPT_PART',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'MODIFICATION_NOMBRE_GUICHETS',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'CREA_COMPT_CONC',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'MODIF_COMPT_CONC',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'INVA_COMPT_CONC',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'ANNULATION',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'MODIF_ADRESSE_CVN',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'CHANG_TIT_NORMAL_CVN',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'CHANG_TIT_DIPLO_CVN',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'CHANG_LOC_CVN',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'PROROG_USAGE_CVN',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'MODIFIER_USAGES_CVN',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'MODIF_ETAT_CIVIL_CVN',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'MODIF_CARAC_TECH_CVN',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'RENOUV_WG_CVN',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'FIN_USAGE_DEMO_FINDEM_CVN',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'FIN_USAGE_DEMO_VENTE_CVN',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'ATT_NUM_USURPATION_CVN',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'ANNULATION_ETATS_ADMINISTRATIFS',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'EXECUTION_REQUETE_ANTS',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'ANN_PROG_REQUETE_ANTS',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'ENR_PROG_REQUETE_ANTS',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'OUTREPASSER_VOL',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'OUTREPASSER_VIN_DOUBLON',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'OUTREPASSER_VIN_NON_CONTROLE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'OUTREPASSER_OTC_INDISPONIBLE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'OUTREPASSER_CT_EXONERATION',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'OUTREPASSER_CT_EXEMPTION',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'OUTREPASSER_EXONERATION_TAXES',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'OUTREPASSER_REFCNIT_RUBVARIABLES',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'OUTREPASSER_REFCNIT_MENTIONVARIABLE',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'BATCH_VEHICULES_IMPORTES',
   },
   {
-    opa_date: '28/04/2023',
+    opa_date: '2023-04-28',
     opa_type: 'EDIT_ETAT_REGIE',
   },
   {
-    opa_date: '16/09/2014',
+    opa_date: '2014-09-16',
     opa_type: 'REIMMAT_ETRANGER',
   },
   {
-    opa_date: '06/11/2013',
+    opa_date: '2013-11-06',
     opa_type: 'DECLARATION_CESSION',
   },
   {
-    opa_date: '15/10/2013',
+    opa_date: '2013-10-15',
     opa_type: 'DECLARATION_ACHAT',
   },
   {
-    opa_date: '04/10/2013',
+    opa_date: '2013-10-04',
     opa_type: 'DECLARATION_CESSION',
   },
   {
-    opa_date: '04/10/2013',
+    opa_date: '2013-10-04',
     opa_type: 'DECLARATION_ACHAT',
   },
   {
-    num_agree: '003205-VE',
-    opa_date: '08/08/2013',
+    opa_date: '2013-08-08',
     opa_type: 'PREM_RAP_VE',
-    id_pve: '1',
   },
   {
-    num_agree: '003205-VE',
-    opa_date: '02/08/2013',
+    opa_date: '2013-08-02',
     opa_type: 'DEC_VE',
-    id_pve: '1',
   },
   {
-    opa_date: '20/09/2010',
+    opa_date: '2010-09-20',
     opa_type: 'CHANG_TIT_NORMAL',
   },
   {
-    opa_date: '23/04/2010',
+    opa_date: '2010-04-23',
     opa_type: 'DECLARATION_ACHAT',
   },
   {
-    opa_date: '29/04/2009',
+    opa_date: '2009-04-29',
     opa_type: 'IMMAT_NORMALE',
   },
 ]
