75b5cc657052731c3af63ed994ebce610cd3b30e
alexrignault <alex.rignault@ext.soprasteria.com>
2023-06-22 17:18:17 +0200

    prise en compte retour: test getTomorrow et getToday

--- frontend/src/tests/unit/utils/date.spec.js
+++ frontend/src/tests/unit/utils/date.spec.js
@@ -1,5 +1,5 @@
 import { describe, expect, test } from 'vitest';
-import { getAnneeEtMois } from '@Utils/date'
+import { getAnneeEtMois, getTodayTime, getTomorrowTime } from '@Utils/date'
 import add from 'date-fns/add'
 import format from 'date-fns/format'
 
@@ -15,6 +15,27 @@ describe('getAnneeEtMois', () => {
     const date = add(new Date(),  {days: -7, months: -2})
     const dateFormate =  format(date, 'yyyyMM')
     expect(getAnneeEtMois(2)).toBe(dateFormate)
+  })
+
+})
+
+describe('getTomorrowTime', () => {
+
+  test('Doit retourner une heure en miliseconde incrémenté de un jour', () => {
+    const date = new Date()
+    date.setDate(date.getDate() + 1)
+    const demain = new Date(date.toDateString()).getTime()
+    expect(getTomorrowTime()).toBe(demain)
+  })
+
+})
+
+describe('getTodayTime', () => {
+
+  test('Doit retourner une heure en miliseconde incrémenté de un jour', () => {
+    const date = new Date()
+    const aujourdhui = new Date(date.toDateString()).getTime()
+    expect(getTodayTime()).toBe(aujourdhui)
   })
 
 })
