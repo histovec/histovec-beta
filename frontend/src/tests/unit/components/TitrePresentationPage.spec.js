16904a8aceff0dd410d088eb465f09e285b0df40
alexrignault <alex.rignault@ext.soprasteria.com>
2023-06-22 17:38:03 +0200

    prise en compte retour: tester les données renseignées dans les balises

--- frontend/src/tests/unit/components/TitrePresentationPage.spec.js
+++ frontend/src/tests/unit/components/TitrePresentationPage.spec.js
@@ -6,9 +6,9 @@ describe('TitrePresentationPage', () => {
   it('Doit afficher une entete de presentation de la page', () => {
     const id = 'id-image';
     const src = 'src-image';
-    const titre = 'titre';
-    const sousTitre = 'sousTitre';
-    const description = 'descritpion';
+    const titre = 'titre de présentation';
+    const sousTitre = 'sousTitre de présentation';
+    const description = 'descritpion de présentation';
 
     const wrapper = mount(TitrePresentationPage, { props: {
         id: id,
@@ -28,10 +28,13 @@ describe('TitrePresentationPage', () => {
 
     const h1 = divTitre.find('h1');
     expect(h1.exists()).true;
+    expect(h1.text()).toBe(wrapper.vm.titre);
     const h2 = divTitre.find('h2');
     expect(h2.exists()).true;
+    expect(h2.text()).toBe(wrapper.vm.sousTitre);
     const p = divTitre.find('p');
     expect(p.exists()).true;
     expect(p.classes('fr-text--xl')).true;
+    expect(p.text()).toBe(wrapper.vm.description);
   })
 })
