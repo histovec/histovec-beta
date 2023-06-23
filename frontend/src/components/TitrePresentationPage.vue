2f8f65f91248fb5f1083f13e0fc287c414cd0932
alexrignault <113456768+alexrignault@users.noreply.github.com>
2023-06-23 09:48:38 +0200

    feat(): refacto titre et image presentation (#1482)

---
+++ frontend/src/components/TitrePresentationPage.vue
@@ -0,0 +1,50 @@
+<script>
+import {defineComponent} from 'vue'
+import ImagePresentation from '@Components/ImagePresentation.vue';
+import '@Assets/stylesheets/image.css'
+
+export default defineComponent({
+  name: 'TitrePresentationPage',
+
+  components: { ImagePresentation },
+
+  props: {
+    id: {
+      type: String,
+      default: null,
+    },
+    titre: {
+      type: String,
+      default: '',
+    },
+    sousTitre: {
+      type: String,
+      default: '',
+    },
+    description: {
+      type: String,
+      default: '',
+    },
+    src: {
+      type: String,
+      default: '',
+    },
+  },
+})
+</script>
+
+<template>
+  <div class="fr-col-lg-4 fr-col-xl-4">
+    <ImagePresentation
+      :id="id"
+      :src="src"
+    />
+  </div>
+  <div class="fr-col-12 fr-col-lg-8 fr-col-xl-8 fr-mt-10v">
+    <h1>{{ titre }}</h1>
+    <h2>{{ sousTitre }}</h2>
+    <p class="fr-text--xl">
+      {{ description }}
+    </p>
+  </div>
+</template>
