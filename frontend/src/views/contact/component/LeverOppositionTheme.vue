4616d51827228813cd4b1aa18642c5c3dd53bc67
alexrignault <113456768+alexrignault@users.noreply.github.com>
2023-06-26 16:57:03 +0200

    feat(): refacto contact page (#1491)
    Co-authored-by: Lucas Ulivi <111894964+lucas-ulivi@users.noreply.github.com>

---
+++ frontend/src/views/contact/component/LeverOppositionTheme.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="fr-grid-row  fr-grid-row--gutters  fr-grid-row--center  fr-mt-4w">
+    <div class="fr-col-12  fr-col-md-10  fr-col-lg-10  fr-col-xl-10  fr-pb-0">
+      <h2 class="fr-h5">
+        Marche à suivre
+      </h2>
+    </div>
+  </div>
+  <div class="fr-grid-row  fr-grid-row--gutters  fr-grid-row--center  fr-mb-4w  fr-mt-0">
+    <div class="fr-col-12  fr-col-offset-md-1  fr-col-md-11  fr-col-offset-lg-1  fr-col-lg-11  fr-col-offset-xl-1  fr-col-xl-11">
+      <p>
+        Il convient de contacter le Centre Amendes Service au 08 21 08 00 31 (appel surtaxé) ou
+        <a
+          class="fr-link"
+          href="https://www.antai.gouv.fr"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          l'Agence nationale de traitement automatisé des infractions (A&#8203;N&#8203;T&#8203;A&#8203;I)
+        </a>.
+      </p>
+    </div>
+  </div>
+</template>
