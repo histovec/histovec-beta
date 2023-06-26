36ff20c9be369fc23c9e7921cfa259e7b1dea3bd
alexrignault <113456768+alexrignault@users.noreply.github.com>
2023-06-26 16:57:03 +0200

    feat(): refacto contact page (#1491)
    Co-authored-by: Lucas Ulivi <111894964+lucas-ulivi@users.noreply.github.com>

---
+++ frontend/src/views/contact/component/CessionVehiculeTheme.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="fr-grid-row  fr-grid-row--gutters  fr-grid-row--center  fr-mt-4w">
+    <div class="fr-col-12  fr-col-md-10  fr-col-lg-10  fr-col-xl-10  fr-pb-0">
+      <h2 class="fr-h5">
+        Marche à suivre
+      </h2>
+    </div>
+  </div>
+
+  <div class="fr-grid-row  fr-grid-row--gutters  fr-grid-row--center  fr-mb-4w  fr-mt-0">
+    <div class="fr-col-12  fr-col-offset-md-1  fr-col-md-11  fr-col-offset-lg-1  fr-col-lg-11  fr-col-offset-xl-1  fr-col-xl-11">
+      <p>
+        Il convient d'effectuer les
+        <a
+          class="fr-link"
+          href="https://immatriculation.ants.gouv.fr/Questions-frequentes/Vendre-ou-donner-mon-vehicule/Commencer-une-declaration-de-cession"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          démarches de déclaration de cession du véhicule
+        </a>
+        auprès de l'A&#8203;N&#8203;T&#8203;S (Agence Nationale des Titres Sécurisés).
+      </p>
+    </div>
+  </div>
+</template>
