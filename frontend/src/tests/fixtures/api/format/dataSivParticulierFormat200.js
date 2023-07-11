e81ef9268e5a9b29e1fbf7a74370940b6673d042
alexrignault <alex.rignault@ext.soprasteria.com>
2023-07-11 09:55:53 +0200

    correction tu niveausonore

--- frontend/src/tests/fixtures/api/format/dataSivParticulierFormat200.js
+++ frontend/src/tests/fixtures/api/format/dataSivParticulierFormat200.js
@@ -24,7 +24,7 @@ export const dataSivParticulierFormat200 = {
       energie: 'Essence',
       nbPlacesAssises: 5,
       nbPlacesDebout: 0,
-      niveauSonore: 0,
+      niveauSonore: 10,
       vitesseMoteur: 4125,
       co2: 140,
       pollution: '70/220*2003/76EURO4',
